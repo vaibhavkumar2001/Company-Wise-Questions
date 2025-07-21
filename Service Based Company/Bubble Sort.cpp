8b94df1f8e0e7478c49f423acb27e4bd64fe6cc7
Vaibhav Kumar <126766355+vaibhavkumar2001@users.noreply.github.com>
2025-07-21 16:18:35 +0530

    Add implementation of optimized Bubble Sort algorithm

---
+++ Service Based Company/Bubble Sort.cpp
@@ -0,0 +1,38 @@
+// Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in the wrong order. This algorithm is not suitable for large data sets as its average and worst-case time complexity are quite high.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// An optimized version of Bubble Sort 
+void bubbleSort(vector<int>& arr) {
+    int n = arr.size();
+    bool swapped;
+  
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+      
+        // If no two elements were swapped, then break
+        if (!swapped)
+            break;
+    }
+}
+
+// Function to print a vector
+void printVector(const vector<int>& arr) {
+    for (int num : arr)
+        cout << " " << num;
+}
+
+int main() {
+    vector<int> arr = { 64, 34, 25, 12, 22, 11, 90 };
+    bubbleSort(arr);
+    cout << "Sorted array: \n";
+    printVector(arr);
+    return 0;
+}
