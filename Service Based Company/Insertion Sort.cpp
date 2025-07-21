efbd09ff771d4d97be23eaa2b4a24f8d9d93366f
Vaibhav Kumar <126766355+vaibhavkumar2001@users.noreply.github.com>
2025-07-21 16:34:52 +0530

    Insertion sort done

---
+++ Service Based Company/Insertion Sort.cpp
@@ -0,0 +1,50 @@
+// Insertion sort is a simple sorting algorithm that works by iteratively inserting each element of an unsorted list into its correct position in a sorted portion of the list. It is like sorting playing cards in your hands. You split the cards into two groups: the sorted cards and the unsorted cards. Then, you pick a card from the unsorted group and put it in the right place in the sorted group.
+
+// We start with the second element of the array as the first element is assumed to be sorted.
+// Compare the second element with the first element if the second element is smaller then swap them.
+// Move to the third element, compare it with the first two elements, and put it in its correct position
+// Repeat until the entire array is sorted.
+
+// C++ program for implementation of Insertion Sort
+#include <iostream>
+using namespace std;
+
+/* Function to sort array using insertion sort */
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; ++i) {
+        int key = arr[i];
+        int j = i - 1;
+
+        /* Move elements of arr[0..i-1], that are
+           greater than key, to one position ahead
+           of their current position */
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+/* A utility function to print array of size n */
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; ++i)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+// Driver method
+int main()
+{
+    int arr[] = { 12, 11, 13, 5, 6 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    insertionSort(arr, n);
+    printArray(arr, n);
+
+    return 0;
+}
+
+/* This code is contributed by Hritik Shah. */
