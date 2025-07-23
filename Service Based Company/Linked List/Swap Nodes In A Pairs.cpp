224ca2d413628ec61357e420b0b1faa39834ba72
Vaibhav Kumar <126766355+vaibhavkumar2001@users.noreply.github.com>
2025-07-23 15:18:25 +0530

    Add detailed comments and examples for the Swap Nodes In Pairs algorithm

--- Service Based Company/Linked List/Swap Nodes In A Pairs.cpp
+++ Service Based Company/Linked List/Swap Nodes In A Pairs.cpp
@@ -1,3 +1,12 @@
+
+// Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+
+// Example 1:
+
+// Input: head = [1,2,3,4]
+
+// Output: [2,1,4,3]
+
 #include<bits/stdc++.h>
 using namespace std;
 class ListNode {
